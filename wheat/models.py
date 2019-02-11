2cbefc2189499b47f5e38ba6afc66c602e604175
Sarah Stensberg <ladystensberg@gmail.com>
2019-02-10 21:35:07 -0800

    added account page and login/signup/logout full functionality

--- wheat/models.py
+++ wheat/models.py
@@ -1,16 +1,24 @@
 from datetime import datetime
-from wheat import db
+from wheat import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
-	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(20), unique=True, nullable=False)
-	email = db.Column(db.String(120), unique=True, nullable=False)
-	password = db.Column(db.String(60), nullable=False)
-	recipes = db.relationship('Recipe', backref='user', lazy=True)
-	# meal_plans = db.relationship('Meal Plan', backref='user', lazy=True)
 
-	def __repr__(self):
-		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    recipes = db.relationship('Recipe', backref='user', lazy=True)
+    # meal_plans = db.relationship('Meal Plan', backref='user', lazy=True)
+
+    def __repr__(self):
+            return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
 
 class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
