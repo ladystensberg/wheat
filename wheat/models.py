d72dc737c06f5020f2649664933672b5470b13fb
Sarah Stensberg <ladystensberg@gmail.com>
2019-02-11 12:21:52 -0800

    created additional models

--- wheat/models.py
+++ wheat/models.py
@@ -9,21 +9,48 @@ def load_user(user_id):
 
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-    recipes = db.relationship('Recipe', backref='user', lazy=True)
-    # meal_plans = db.relationship('Meal Plan', backref='user', lazy=True)
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(20), unique=True, nullable=False)
+	email = db.Column(db.String(120), unique=True, nullable=False)
+	password = db.Column(db.String(60), nullable=False)
+	recipe_box = db.relationship('RecipeBox', backref='user', lazy=True)
+	pantry = db.relationship('Pantry', backref='user', lazy=True)
+	# meal_plans = db.relationship('Meal Plan', backref='user', lazy=True)
 
-    def __repr__(self):
-            return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+	def __repr__(self):
+		return f"User('{self.username}', '{self.email})"
+
+
+class RecipeBox(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+	recipes = db.relationship('Recipe', backref='recipe_box', lazy=True)
+
+	def __repr__(self):
+		return f"Recipe Box('{self.user_id}')"
 
 
 class Recipe(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    source_url = db.Column(db.String)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+	id = db.Column(db.Integer, primary_key=True)
+	spoonacular_id = db.Column(db.Integer)
+	recipe_box_id = db.Column(db.Integer, db.ForeignKey(
+	    'recipe_box.id'), nullable=False)
 
-    def __repr__(self):
-        return f"Post('{self.source_url}')"
+	def __repr__(self):
+		return f"Recipe('{self.spoonacular_id}')"
+
+class Pantry(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+	ingredients = db.relationship('Ingredient', backref='pantry', lazy=True)
+
+	def __repr__(self):
+		return f"Pantry('{self.id}')"
+
+class Ingredient(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	ingredient_name = db.Column(db.String(40), nullable=False)
+	pantry_id = db.Column(db.Integer, db.ForeignKey('pantry.id'), nullable=False)
+
+	def __repr__(self):
+		return f"Recipe('{self.ingredient_name}')"
